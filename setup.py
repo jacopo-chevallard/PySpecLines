9768f3f3ffb514a0f3f8fa4979c3dd79a2c34b67
Jacopo Chevallard <jacopo.chevallard@gmail.com>
2019-02-14 13:08:52 +0100

    Added required dependencies to correct for Galactic extinction

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['matplotlib', 'numpy', 'astropy<3', 'pyspeckit'],
+    install_requires=['matplotlib', 'numpy', 'astropy<3', 'pyspeckit', 'dustmaps', 'extinction'],
 
     include_package_data=True, 
 
